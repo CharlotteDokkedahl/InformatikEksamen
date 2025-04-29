1c88ceaba3bf81eef95ae274dc24610dac251995
CharlotteDokkedahl <Charlottedokkedahl@gmail.com>
2025-04-29 15:20:51 +0200

    relativt okay version

--- proeve.js
+++ proeve.js
@@ -1,3 +1,11 @@
+// Variabler til isterninger
+let xIs1 = -10;
+let xIs2 = 360;
+const xIs1Slut = 60;
+const xIs2Slut = 230;
+let flytIs = false;
+
+// Arrays til cirkler
 let mangeCirklerLille = [];
 let mangeCirklerStor = [];
 let nyCirkler = [];
@@ -5,90 +13,112 @@ let orangeCirkler = [];
 
 let antalCirklerLille = 10;
 let antalCirklerStor = 10;
-let antalOrangeCirkler = 5;
+let antalOrangeCirkler = 2
+;
+
+let icecube;
+
+function preload() {
+  icecube = loadImage("icecubes.png");
+}
 
 function setup() {
   createCanvas(870, 450);
+  
+  createButton("Større volume").position(740, 370).size(110, 30);
+  createButton("Mindre volume").position(740, 400).size(110, 30);
+  createButton("Tilsæt Fe3+").position(620, 370).size(110, 30);
+  createButton("Tilsæt SCN-").position(620, 400).size(110, 30);
+  createButton("Tilføj varme").position(500, 370).size(110, 30);
+  createButton("Tilføj kulde").position(500, 400).size(110, 30).mousePressed(startFlytIs);
 
-  for (let i = 0; i < antalCirklerLille; i++) {
-    mangeCirklerLille.push(new Cirkler(3));
-  }
-
-  for (let j = 0; j < antalCirklerStor; j++) {
-    mangeCirklerStor.push(new Cirkler(3));
-  }
-
-  for (let o = 0; o < antalOrangeCirkler; o++) {
-    orangeCirkler.push(new Cirkler(5));
-  }
+  for (let i = 0; i < antalCirklerLille; i++) mangeCirklerLille.push(nyCirkel(3));
+  for (let j = 0; j < antalCirklerStor; j++) mangeCirklerStor.push(nyCirkel(3));
+  for (let o = 0; o < antalOrangeCirkler; o++) orangeCirkler.push(nyCirkel(5));
 }
 
 function draw() {
+
   background(220);
+  fyld();
 
-  // små lilla
-  for (let i = mangeCirklerLille.length - 1; i >= 0; i--) {
-    mangeCirklerLille[i].TegnCirkel(100, 200, 170);
-    mangeCirklerLille[i].FlytCirkel();
-    mangeCirklerLille[i].Glas();
+  tegnOgFlytAlle(mangeCirklerLille,100, 200, 170);
+  tegnOgFlytAlle(mangeCirklerStor,150, 100, 250);
+  tegnOgFlytAlle(nyCirkler,200, 0, 0);
+  tegnOgFlytAlle(orangeCirkler,250,250,250);
+
+  reaktionLilleOgStor();
+  reaktionOrangeOgRod();
+
+  visGUI();
+  tegnIs();
+  opdaterIs();
+
+}
+
+function nyCirkel(r) {
+  const x = random(155, 325);
+  const y = random(175, 365);
+  return {
+    x, y, r,
+    vx: random(-3, 3),
+    vy: random(-3, 3),
+  };
+}
+
+function tegnOgFlytAlle(arr,r,g,b) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    fill(r,g,b);
+    noStroke();
+    circle(arr[i].x, arr[i].y, arr[i].r * 2);
+    flyt(arr[i]);
+    if (arr === mangeCirklerLille && i === 0) tegnGlas();
   }
+}
 
-  // store grønne
-  for (let j = mangeCirklerStor.length - 1; j >= 0; j--) {
-    mangeCirklerStor[j].TegnCirkel(150, 100, 250);
-    mangeCirklerStor[j].FlytCirkel();
-  }
+function flyt(c) {
+  c.x += c.vx;
+  c.y += c.vy;
+  if (c.x < 150 + c.r || c.x > 330 - c.r) c.vx *= -1;
+  if (c.y < 170 + c.r || c.y > 370 - c.r) c.vy *= -1;
+}
 
-  // røde
-  for (let k = nyCirkler.length - 1; k >= 0; k--) {
-    nyCirkler[k].TegnCirkel(200, 0, 0);
-    nyCirkler[k].FlytCirkel();
-  }
+function afstand(a, b) {
+  return dist(a.x, a.y, b.x, b.y);
+}
 
-  // orange
-  for (let o = orangeCirkler.length - 1; o >= 0; o--) {
-    orangeCirkler[o].TegnCirkel(250);
-    orangeCirkler[o].FlytCirkel();
-  }
-
-  // reaktion mellem lilla og grøn => rød
+function reaktionLilleOgStor() {
   for (let i = mangeCirklerLille.length - 1; i >= 0; i--) {
     for (let j = mangeCirklerStor.length - 1; j >= 0; j--) {
-      if (mangeCirklerLille[i].Interaktion(mangeCirklerStor[j])) {
-        // lav en ny rød cirkel midt imellem
-        let midX = (mangeCirklerLille[i].xCirkel + mangeCirklerStor[j].xCirkel) / 2;
-        let midY = (mangeCirklerLille[i].yCirkel + mangeCirklerStor[j].yCirkel) / 2;
-
-        let ny = new Cirkler(5);
-        ny.xCirkel = midX;
-        ny.yCirkel = midY;
+      if (afstand(mangeCirklerLille[i], mangeCirklerStor[j]) < mangeCirklerLille[i].r + mangeCirklerStor[j].r) {
+        const midX = (mangeCirklerLille[i].x + mangeCirklerStor[j].x) / 2;
+        const midY = (mangeCirklerLille[i].y + mangeCirklerStor[j].y) / 2;
+        const ny = nyCirkel(5);
+        ny.x = midX;
+        ny.y = midY;
         nyCirkler.push(ny);
-
         mangeCirklerLille.splice(i, 1);
         mangeCirklerStor.splice(j, 1);
         break;
       }
     }
   }
+}
 
-  // reaktion mellem orange og rød => lilla og grøn
+function reaktionOrangeOgRod() {
   for (let o = 0; o < orangeCirkler.length; o++) {
     for (let r = nyCirkler.length - 1; r >= 0; r--) {
-      if (orangeCirkler[o].Interaktion(nyCirkler[r])) {
-        let rx = nyCirkler[r].xCirkel;
-        let ry = nyCirkler[r].yCirkel;
-
-        let nyLilla = new Cirkler(3);
-        nyLilla.xCirkel = rx + random(-5, 5);
-        nyLilla.yCirkel = ry + random(-5, 5);
-
-        let nyGrøn = new Cirkler(3);
-        nyGrøn.xCirkel = rx + random(-5, 5);
-        nyGrøn.yCirkel = ry + random(-5, 5);
-
+      if (afstand(orangeCirkler[o], nyCirkler[r]) < orangeCirkler[o].r + nyCirkler[r].r) {
+        const rx = nyCirkler[r].x;
+        const ry = nyCirkler[r].y;
+        let nyLilla = nyCirkel(3);
+        nyLilla.x = rx + random(-5, 5);
+        nyLilla.y = ry + random(-5, 5);
+        let nyGron = nyCirkel(3);
+        nyGron.x = rx + random(-5, 5);
+        nyGron.y = ry + random(-5, 5);
         mangeCirklerLille.push(nyLilla);
-        mangeCirklerStor.push(nyGrøn);
-
+        mangeCirklerStor.push(nyGron);
         nyCirkler.splice(r, 1);
         break;
       }
@@ -96,50 +126,48 @@ function draw() {
   }
 }
 
-class Cirkler {
-  constructor(r) {
-    this.GlasP1x = 120;
-    this.GlasP1y = 200;
-    this.GlasP2x = 120;
-    this.GlasP2y = 400;
-    this.GlasP3x = 320;
-    this.GlasP3y = 400;
-    this.GlasP4x = 320;
-    this.GlasP4y = 200;
-
-    this.xCirkel = random(this.GlasP1x + 5, this.GlasP4x - 5);
-    this.yCirkel = random(this.GlasP1y + 5, this.GlasP2y - 5);
-    this.rCirkel = r;
-    this.speedX = random(-3, 3);
-    this.speedY = random(-3,3);
-  }
-
-  TegnCirkel(red, green, blue) {
-    fill(red, green, blue);
-    noStroke();
-    circle(this.xCirkel, this.yCirkel, this.rCirkel * 2);
-  }
-
-  FlytCirkel() {
-    this.xCirkel += this.speedX;
-    this.yCirkel += this.speedY;
-
-    if (this.xCirkel < this.GlasP1x + this.rCirkel) this.speedX *= -1;
-    if (this.xCirkel > this.GlasP4x - this.rCirkel) this.speedX *= -1;
-    if (this.yCirkel < this.GlasP1y + this.rCirkel) this.speedY *= -1;
-    if (this.yCirkel > this.GlasP2y - this.rCirkel) this.speedY *= -1;
-  }
-
-  Interaktion(anden) {
-    let distance = dist(this.xCirkel, this.yCirkel, anden.xCirkel, anden.yCirkel);
-    return this.rCirkel + anden.rCirkel > distance;
-  }
-
-  Glas() {
-    stroke(0);
-    strokeWeight(2);
-    line(this.GlasP1x, this.GlasP1y, this.GlasP2x, this.GlasP2y);
-    line(this.GlasP2x, this.GlasP2y, this.GlasP3x, this.GlasP3y);
-    line(this.GlasP3x, this.GlasP3y, this.GlasP4x, this.GlasP4y);
-  }
+function tegnGlas() {
+  stroke(0);
+  strokeWeight(2);
+  line(150, 170, 150, 370);
+  line(150, 370, 330, 370);
+  line(330, 370, 330, 170);
 }
+
+function tegnIs() {
+  icecube.resize(115, 100);
+  image(icecube, xIs1, 300);
+  image(icecube, xIs2, 300);
+}
+
+function opdaterIs() {
+  if (flytIs) {
+    if (abs(xIs1 - xIs1Slut) > 1) xIs1++;
+    if (abs(xIs2 - xIs2Slut) > 1) xIs2--;
+  }
+  if (abs(xIs1 - xIs1Slut) <= 1 && abs(xIs2 - xIs2Slut) <= 1) flytIs = false;
+}
+
+function startFlytIs() {
+  flytIs = true;
+}
+
+function visGUI() {
+  fill(0);
+  textSize(20);
+  text("Teori og brugervejledning", 510, 35);
+  text("Tempratur:", 505, 360);
+  text("Stoffer:", 625, 360);
+  text("Volume:", 745, 360);
+}
+
+function fyld() {
+  let rød=200;
+ if (nyCirkler.length>4){ 
+  rød=255;
+ }
+ fill(rød,100,100,150);
+ rect(150,170,180,200);
+
+}
+

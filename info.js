ae059c00d987c22c40aa6882d18106d47be556ee
Felix_H_22r <143402770+felixH1801@users.noreply.github.com>
2025-04-29 14:31:33 +0200

    ny

--- info.js
+++ info.js
@@ -4,41 +4,56 @@ xIs2 = 360;
 xIs1Slut = 60;
 xIs2Slut = 230;
 flytIs = false;
+ildTaend = true;
+storeVolume = false;
+mindreVolume = false;
+
+GlasP1x = 150; //150
+GlasP1y = 170; //170
+GlasP2x = 150; //150
+GlasP2y = 370; //370
+GlasP3x = 330; //330
+GlasP3y = 370; //370
+GlasP4x = 330; //330
+GlasP4y = 170; //170
 
 function preload()
 {
-icecube = loadImage('icecubes.png');
+    icecube = loadImage('icecubes.png');
+    ild = loadImage('ild.png');
 }
 
 function setup() 
 {
-createCanvas(870, 450);
+    createCanvas(870, 450);
 
-let button1 = createButton('Større volume');
-button1.position(740, 370);
-button1.size(110,30);
+    let button1 = createButton('Større volume');
+    button1.position(760, 390);
+    button1.size(110,30);
+    button1.mousePressed(startStore);
 
-let button2 = createButton('Mindre volume');
-button2.position(740, 400);
-button2.size(110,30);
+    let button2 = createButton('Mindre volume');
+    button2.position(760, 420);
+    button2.size(110,30);
+    button2.mousePressed(startMindre);
 
-let button3 = createButton('Tilsæt Fe3+');
-button3.position(620, 370);
-button3.size(110,30);
+    let button3 = createButton('Tilsæt Fe3+');
+    button3.position(640, 390);
+    button3.size(110,30);
 
-let button4 = createButton('Tilsæt SCN-');
-button4.position(620, 400);
-button4.size(110,30);
+    let button4 = createButton('Tilsæt SCN-');
+    button4.position(640, 420);
+    button4.size(110,30);
 
-let button5 = createButton('Tilføj varme');
-button5.position(500, 370);
-button5.size(110,30);
-button5.mousePressed();
+    let button5 = createButton('Tilføj varme');
+    button5.position(520, 390);
+    button5.size(110,30);
+    button5.mousePressed(startIldTaend);
 
-let button6 = createButton('Tilføj kulde');
-button6.position(500, 400);
-button6.size(110,30);
-button6.mousePressed(startFlytIs)
+    let button6 = createButton('Tilføj kulde');
+    button6.position(520, 420);
+    button6.size(110,30);
+    button6.mousePressed(startFlytIs);
 
 }
 
@@ -46,15 +61,9 @@ function draw()
 {
 background(220);
 
+fill(255);
+stroke(0);
 rect(500,50,350,270);
-GlasP1x = 150;
-GlasP1y = 170;
-GlasP2x = 150;
-GlasP2y = 370;
-GlasP3x = 330;
-GlasP3y = 370;
-GlasP4x = 330;
-GlasP4y = 170;
   line(GlasP1x,GlasP1y,GlasP2x,GlasP2y);
   line(GlasP2x,GlasP2y,GlasP3x,GlasP3y);
   line(GlasP3x,GlasP3y,GlasP4x,GlasP4y);
@@ -71,6 +80,8 @@ icecube.resize(115,100);
 image(icecube, xIs1, 300);
 image(icecube, xIs2, 300);
 
+ild.resize(110,110);
+image(ild, 185, 360);
 
 //Starter isen
 if (flytIs) 
@@ -96,9 +107,54 @@ if(abs(xIs2 - xIs2Slut) <= 1)
     flytIs = false;
   }
 
+if(ildTaend)
+  {
+    fill(220);
+    noStroke();
+    rect(225,375,30,30);
+  }
+
+if(storeVolume)
+  {
+    GlasP1x = 120;
+    GlasP1y = 170;
+    GlasP2x = 120;
+    GlasP2y = 370;
+    GlasP3x = 360;
+    GlasP3y = 370;
+    GlasP4x = 360;
+    GlasP4y = 170;
+  }
+
+if(mindreVolume)
+  {
+    GlasP1x = 180;
+    GlasP1y = 170;
+    GlasP2x = 180;
+    GlasP2y = 370;
+    GlasP3x = 300;
+    GlasP3y = 370;
+    GlasP4x = 300;
+    GlasP4y = 170;
+  }
 }
 
 function startFlytIs()
 {
 flytIs = true;
+}
+
+function startIldTaend()
+{
+ildTaend = false;
+}
+
+function startStore()
+{
+storeVolume = true;
+}
+
+function startMindre()
+{
+mindreVolume = true;
 }
